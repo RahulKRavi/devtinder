d4bef0261234befd9bd80c8974676e3472c5a3c0
RahulKRavi <rahulkr02042000@gmail.com>
2025-01-17 23:08:19 +0530

    feed data

--- src/app.js
+++ src/app.js
@@ -1,19 +1,13 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 const mongoose = require('mongoose')
 const connectDB = mongoose.connect("mongodb+srv://rahulkr02042000:gfceHgXKD3L4ZAUH@violetbooks.7suo0.mongodb.net/devTinder")
 const User = require('./models/userModel')
 
 app.post('/signup', async (req, res) => {
   try {
-    const user = new User({
-      firstName: "Rahul",
-      lastName: "Ravi",
-      email: "fuckyou@gmail.com",
-      password: "8989",
-      age: 23,
-      gender: "Male"
-    })
+    const user = new User(req.body)
     await user.save()
     res.send("User added successfully")
   } catch(err) {
@@ -22,6 +16,18 @@ app.post('/signup', async (req, res) => {
   }
 })
 
+app.get('/feed', async (req, res) => {
+  try {
+    const users = await User.findOne({email: req.body.email})
+    if(users.length !== 0){
+      res.send(users)
+    } else {
+      res.status(404).send("User not found")
+    }
+  } catch(err) {
+    res.status(401).send("Something shit happens")
+  }
+})
 connectDB.then(()=>{
   console.log("Database Connected Succesfully")
   app.listen(2222,()=>{
